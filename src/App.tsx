e86c349ff10b5ca272498e6d9f90c9368052b554
Rasulwebs <boboqulovrasul80@gmail.com>
2024-01-23 15:29:16 +0500

    mini edit

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,6 @@ import { ForgotPassword } from "./pages/forgotPassword";
 
 // const API_URL = "https://api.nestjs-query.refine.dev/graphql";
 // const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
-
 // const gqlClient = new GraphQLClient(API_URL);
 // const wsClient = createClient({ url: WS_URL });
 
