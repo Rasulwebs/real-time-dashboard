183547bde93a72e7a4d61a9b0b7150ad00f9b14b
Rasulwebs <boboqulovrasul80@gmail.com>
2024-01-23 15:22:00 +0500

    started real time dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,83 @@
-import { 
-    Refine,
-    GitHubBanner, 
-    WelcomePage,
-    Authenticated, 
-} from '@refinedev/core';
+import {
+  Refine,
+  GitHubBanner,
+  WelcomePage,
+  Authenticated,
+} from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
-import { AuthPage,ErrorComponent
-,useNotificationProvider
-,ThemedLayoutV2
-,ThemedSiderV2} from '@refinedev/antd';
+import {
+  AuthPage,
+  ErrorComponent,
+  useNotificationProvider,
+  ThemedLayoutV2,
+  ThemedSiderV2,
+} from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import dataProvider, { GraphQLClient, liveProvider } from "@refinedev/nestjs-query";
+// import dataProvider, {
+//   GraphQLClient,
+//   liveProvider,
+// } from "@refinedev/nestjs-query";
+
+import { dataProvider, liveProvider } from "./providers";
 import { createClient } from "graphql-ws";
-import { App as AntdApp } from "antd"
+import { App as AntdApp } from "antd";
 import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
-import routerBindings, { NavigateToResource, CatchAllNavigate, UnsavedChangesNotifier, DocumentTitleHandler } from "@refinedev/react-router-v6";
-import { ColorModeContextProvider } from "./contexts/color-mode";
-import { Header } from "./components/header";
+import routerBindings, {
+  NavigateToResource,
+  CatchAllNavigate,
+  UnsavedChangesNotifier,
+  DocumentTitleHandler,
+} from "@refinedev/react-router-v6";
+
 import { Login } from "./pages/login";
 import { Register } from "./pages/register";
 import { ForgotPassword } from "./pages/forgotPassword";
-import { authProvider } from "./authProvider";
 
-const API_URL = "https://api.nestjs-query.refine.dev/graphql";
-const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
+// const API_URL = "https://api.nestjs-query.refine.dev/graphql";
+// const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
 
-const gqlClient = new GraphQLClient(API_URL);
-const wsClient = createClient({ url: WS_URL });
-
-
-
+// const gqlClient = new GraphQLClient(API_URL);
+// const wsClient = createClient({ url: WS_URL });
 
 function App() {
-    
-
-    
-    
-    return (
-        <BrowserRouter>
-        <GitHubBanner />
-        <RefineKbarProvider>
-            <ColorModeContextProvider>
-<AntdApp>
-            <DevtoolsProvider>
-                <Refine dataProvider={dataProvider(gqlClient)}
-liveProvider={liveProvider(wsClient)}
-notificationProvider={useNotificationProvider}
-routerProvider={routerBindings}
-authProvider={authProvider} 
-                    options={{
-                        syncWithLocation: true,
-                        warnWhenUnsavedChanges: true,
-                        useNewQueryKeys: true,
-                            projectId: "OZiKb3-aObP0u-Th2TDX",
-                        liveMode: "auto",
-                    }}
-                >
-
-
-                        <Routes>
-                            <Route index element={<WelcomePage />} />
-                        </Routes>
-                    <RefineKbar />
-                    <UnsavedChangesNotifier />
-                    <DocumentTitleHandler />
-                </Refine>
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <AntdApp>
+          <DevtoolsProvider>
+            <Refine
+              //   dataProvider={dataProvider(gqlClient)}
+              //   liveProvider={liveProvider(wsClient)}
+              dataProvider={dataProvider}
+              liveProvider={liveProvider}
+              notificationProvider={useNotificationProvider}
+              routerProvider={routerBindings}
+              //   authProvider={authProvider}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                useNewQueryKeys: true,
+                projectId: "OZiKb3-aObP0u-Th2TDX",
+                liveMode: "auto",
+              }}
+            >
+              <Routes>
+                <Route index element={<WelcomePage />} />
+              </Routes>
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
             <DevtoolsPanel />
-            </DevtoolsProvider>
-            </AntdApp>
-</ColorModeContextProvider>
-        </RefineKbarProvider>
-        </BrowserRouter>
-      );
-};
+          </DevtoolsProvider>
+        </AntdApp>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
 
 export default App;
