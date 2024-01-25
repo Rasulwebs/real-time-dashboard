a1607d36341c635ce34e38829ce57b5d9b5a877a
Rasulwebs <boboqulovrasul80@gmail.com>
2024-01-25 18:07:38 +0500

    formated file

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,15 +1,12 @@
-import {
-    AuthPage,
-} from "@refinedev/antd";
+import { AuthPage } from "@refinedev/antd";
 import { authCredentials } from "../../providers";
 
 export const Login = () => {
-    return (
-        <AuthPage
-            type="login"
-            // formProps={{ initialValues:{ email: "demo@refine.dev", password: "demodemo" } }}
-            formProps={{ initialValues:authCredentials }}
-
-        />
-    );
+  return (
+    <AuthPage
+      type="login"
+      // formProps={{ initialValues:{ email: "demo@refine.dev", password: "demodemo" } }}
+      formProps={{ initialValues: authCredentials }}
+    />
+  );
 };
