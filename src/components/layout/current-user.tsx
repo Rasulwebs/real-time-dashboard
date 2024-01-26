66a4bfad36a7fd4196e74477eef813663df38d14
Rasulwebs <boboqulovrasul80@gmail.com>
2024-01-26 17:46:19 +0500

    edited gql file and structur

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,10 +1,10 @@
 import { Popover } from "antd";
 import CustomAvatar from "../custom-avatar";
-// import { useGetIdentity } from "@refinedev/core";
+import { useGetIdentity } from "@refinedev/core";
 
-// import type { User } from "@graphql/schema.types";
+import type { User } from "@/graphql/schema.types";
 const CurrentUser = () => {
-  // const { data: user } = useGetIdentity<User>();
+  const { data: user } = useGetIdentity<User>();
   return (
     <>
       <Popover
--- src/graphql/mutation.ts
+++ src/graphql/mutation.ts
@@ -2,10 +2,9 @@ import gql from "graphql-tag";
 
 // Mutation to update user
 export const UPDATE_USER_MUTATION = gql`
-  # The ! after the type means that it is required
+  # The ! after the type  =  required
   mutation UpdateUser($input: UpdateOneUserInput!) {
     # call the updateOneUser mutation with the input and pass the $input argument
-    # $variableName is a convention for GraphQL variables
     updateOneUser(input: $input) {
       id
       name
